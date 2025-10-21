9405ee8d6aebc485f2c128e27e3153b4f2105183
Bruno Felipe <brunomafraf@gmail.com>
2025-10-21 09:36:55 -0300

    Debug

--- Ex001/Main.py
+++ Ex001/Main.py
@@ -1,10 +1,4 @@
 from Functions import *
-# !qfdPUXJqjXl
-
-# decrypt_caesar=caesar("Hello, There!",0)
-# print(caesar("Hello, There!"))
-# print(decrypt_caesar)
-# print()
 
 vigenere()
-
+caesar()
--- ex002/main.py
+++
@@ -1,2 +0,0 @@
-from Functions import *
-main()
